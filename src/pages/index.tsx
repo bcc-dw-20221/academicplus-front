e5a793450e8eff1fc5c6963c2bc77cb030f8d645
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-08-29 10:51:27 -0300

    add tag main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import Head from "next/head";
 
 export default function PageIndex() {
   return (
-    <div>
+    <>
       <Head>
         <title>Academic Plus</title>
       </Head>
-      <h1>Academic Plus</h1>;
-    </div>
+      <main>
+        <h1>Academic Plus</h1>
+      </main>
+    </>
   );
 }
