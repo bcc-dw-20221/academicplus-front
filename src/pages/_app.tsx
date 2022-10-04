a451303ab8851a921842f79724f5a9fed5c0e368
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-10-04 09:11:55 -0300

    Create pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
-import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/provider";
-import Header from "../components/Header";
-
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "../styles/theme";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/router";
+import { ThemeProvider } from "next-themes";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { ThemeProvider as UiThemeProvider } from "@mui/material/styles";
+import "react-toastify/dist/ReactToastify.css";
+
+import "../styles/globals.css";
+import Header from "../components/Header";
+import { theme } from "../styles/theme";
 import { PATHS } from "../utils/constants";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <ThemeProvider theme={theme}>
-      <ChakraProvider>
-        {router.pathname === PATHS.LOGIN ? "" : <Header />}
+    <ThemeProvider>
+      <UiThemeProvider theme={theme}>
+        <ChakraProvider>
+          {router.pathname === PATHS.LOGIN ? "" : <Header />}
 
-        <ToastContainer />
-        <Component {...pageProps} />
-      </ChakraProvider>
+          <ToastContainer />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </UiThemeProvider>
     </ThemeProvider>
   );
 }
