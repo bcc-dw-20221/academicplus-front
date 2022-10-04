8b1efabac5d483a96d9f6a212d544534e813aee8
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-10-03 22:44:28 -0300

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/src/**/*.tsx"],
+  content: ["./src/**/*.tsx"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#45DAFF",
+          200: "#06B7E4",
+          350: "#0E83A1",
+          400: "#0D99FF",
+          500: "#007FFE",
+          600: "#005AB3",
+          800: "#0A2463",
+          900: "#2F2E41",
+        },
+        success: {
+          hover: "#39C64F",
+          disabled: "#55A361",
+          default: "#2E993F",
+        },
+        cancel: {
+          hover: "#DF1F24",
+          disabled: "#BD5356",
+          default: "#C7181D",
+        },
+        white: "#fefeff",
+        black: "#2c2c2c",
+      },
+    },
   },
   plugins: [],
 };
