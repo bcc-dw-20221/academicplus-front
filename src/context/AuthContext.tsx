23f362b75582d5ceeedd517934505c522f9363bd
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-11-24 14:50:49 -0300

    chore: refactor and add typing

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,41 +1,78 @@
-import { createContext, useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import Router, { useRouter } from "next/router";
 
 import { setCookie, parseCookies, destroyCookie } from "nookies";
 
+import { UserModel } from "../model/UserModel";
 import { toast } from "react-toastify";
 import parseJwt from "../utils/parseJwt";
 import { api } from "../services/axios";
-import { singOut } from "../utils/singOut";
-import { env } from "process";
 
+interface AuthProviderProps {
+  children: ReactNode;
+}
 interface SignInProps {
-  email: string;
+  username: string;
   password: string;
 }
 
-const AuthContext = createContext({});
+export interface InitialValues {
+  user: UserModel;
+  setUser: (user: UserModel) => void;
+  signIn: ({ username, password }: SignInProps) => void;
+  authenticated: boolean;
+  setAuthenticated: (boolean: boolean) => void;
+  isLoading: boolean;
+  setIsLoading: (boolean: boolean) => void;
+  signOut: () => void;
+}
 
-function AuthProvider({ children }: any) {
-  const [user, setUser] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+const initialValue = {
+  user: {
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    permission: ""
+  },
+  setUser: (newState: UserModel) => {},
+  signIn: ({ username, password }: SignInProps) => {},
+  authenticated: false,
+  setAuthenticated: (boolean: boolean) => {},
+  isLoading: false,
+  setIsLoading: (boolean: boolean) => {},
+  signOut: () => {}
+};
+
+export const AuthContext = createContext<InitialValues>(initialValue);
+
+export function AuthProvider({ children }: any) {
+  const [user, setUser] = useState<UserModel>({
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    permission: ""
+  });
   const [authenticated, setAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
-
   console.log(process.env.API_BASEURL);
-
   console.log(user);
 
+  destroyCookie({}, "token");
   useEffect(() => {
     const { "nextauth.token": token } = parseCookies();
+
+    console.log({ token });
 
     if (token) {
       const userCookies = parseJwt(token);
 
-      try {
-      } catch (error) {}
+      console.log({ userCookies });
 
-      setIsLoading(true);
+      setAuthenticated(true);
       setUser(userCookies);
 
       return setUser((prevState: any) => ({ ...prevState, name: "Alexandre" }));
@@ -43,17 +80,33 @@ function AuthProvider({ children }: any) {
   }, []);
 
   const signOut = () => {
-    setIsLoading(false);
-    setUser(undefined);
-    singOut();
+    destroyCookie(undefined, "nextauth.token");
+    setUser({
+      id: "",
+      name: "",
+      username: "",
+      email: "",
+      permission: ""
+    });
+
+    setAuthenticated(false);
+    return Router.push("/login");
   };
 
-  async function singIn({ email, password }: SignInProps) {
+  async function signIn({ username, password }: SignInProps) {
     try {
-      const res = await api.post("http://localhost:8080/", {
-        email,
+      console.log("Enviado");
+      const res = await api.post("/login", {
+        username,
         password
       });
+
+      console.log("Enviado");
+      console.log(res.status);
+      console.log(res.data);
+
+      setAuthenticated(res.status === 200);
+      console.log({ res });
 
       const { token } = res.data;
 
@@ -65,23 +118,33 @@ function AuthProvider({ children }: any) {
         path: "/"
       });
 
-      setUser(parseJwt(token));
-
       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      setUser((prevState: any) => ({ ...prevState, name: "Alexandre" }));
 
       router.push("/");
     } catch (err: any) {
-      toast.error(err.response.data.error, {
-        autoClose: 2000
-      });
+      console.log(err);
+      // toast.error(err.response.data.error, {
+      //   autoClose: 2000
+      // });
     }
   }
 
   return (
-    <AuthContext.Provider value={{ singIn, user, setUser, isLoading, signOut }}>
+    <AuthContext.Provider
+      value={{
+        signIn,
+        user,
+        setUser,
+        authenticated,
+        setAuthenticated,
+        isLoading,
+        setIsLoading,
+        signOut
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
 }
-
-export { AuthContext, AuthProvider };
