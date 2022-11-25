2fa207b9ad18b6bf51428735b6c6f89da69f4623
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-11-25 15:51:13 -0300

    fix: alter permissions according API

--- src/components/ProvisionalPermission.tsx
+++ src/components/ProvisionalPermission.tsx
@@ -15,56 +15,60 @@ export function ProvisionalPermission() {
   const { user, setUser } = useContext<any>(AuthContext);
 
   function handleTogglePermission(str: any) {
-    setUser((prevState: any) => ({ ...prevState, permission: str }));
+    setUser((prevState: any) => ({ ...prevState, permission: [str] }));
   }
 
   const [toggleMenuPermission, setToggleMenuPermission] = useState(false);
   return (
     <>
-    {user ? 
-    <div className="absolute top-3 left-2">
-      <div className="relative">
-        <button
-          onClick={() => setToggleMenuPermission(!toggleMenuPermission)}
-          className="bg-primary-500 text-zinc-50 p-2 rounded-full"
-          >
-          <IdentificationBadge size={32} />
-        </button>
-        {toggleMenuPermission ? (
-          <ul
-          onMouseLeave={() => setToggleMenuPermission(false)}
-            className="absolute top-0 left-14 p-2 bg-primary-500 text-zinc-50 font-medium rounded shadow-md border-2 border-primary-400/50 whitespace-nowrap"
+      {user ? (
+        <div className="absolute top-3 left-2">
+          <div className="relative">
+            <button
+              onClick={() => setToggleMenuPermission(!toggleMenuPermission)}
+              className="bg-primary-500 text-zinc-50 p-2 rounded-full"
             >
-            <li>
-              <button onClick={() => handleTogglePermission("student")}>
-                Aluno
-              </button>
-            </li>
-            <li>
-              <button onClick={() => handleTogglePermission("teacher")}>
-                Professor
-              </button>
-            </li>
-            <li>
-              <button onClick={() => handleTogglePermission("coordinator")}>
-                Coordenador
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => handleTogglePermission("education_manager")}
+              <IdentificationBadge size={32} />
+            </button>
+            {toggleMenuPermission ? (
+              <ul
+                onMouseLeave={() => setToggleMenuPermission(false)}
+                className="absolute top-0 left-14 p-2 bg-primary-500 text-zinc-50 font-medium rounded shadow-md border-2 border-primary-400/50 whitespace-nowrap"
               >
-                Gestor de Ensino
-              </button>
-            </li>
-          </ul>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  : ""  
-  }
+                <li>
+                  <button
+                    onClick={() => handleTogglePermission("ROLE_STUDENT")}
+                  >
+                    Aluno
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => handleTogglePermission("ROLE_PROFESSOR")}
+                  >
+                    Professor
+                  </button>
+                </li>
+
+                <li>
+                  <button onClick={() => handleTogglePermission("ROLE_ADMIN")}>
+                    Gestor de Ensino
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => handleTogglePermission("ROLE_ROOT")}>
+                    Root
+                  </button>
+                </li>
+              </ul>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
